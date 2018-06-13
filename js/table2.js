30c56878c306b6c4705603b87a1660ab584ab6d0
森川隆之 <morio@morikawatakayukinoMacBook-puro.local>
2018-06-14 07:33:11 +0900

    init

---
+++ js/table2.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "./data/griddata.json"
+    })
+    .done( (data) => {
+        var countries = [
+        { Name: "", Id: 0 },
+        { Name: "United States", Id: 1 },
+        { Name: "Canada", Id: 2 },
+        { Name: "United Kingdom", Id: 3 }
+        ];
+
+        $("#jsGrid").jsGrid({
+        width: "100%",
+        height: "400px",
+
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+
+        data: data,
+
+        fields: [
+            { name: "Name", type: "text", width: 150, validate: "required" },
+            { name: "Age", type: "number", width: 50 },
+            { name: "Address", type: "text", width: 200 },
+            { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
+            { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+            { type: "control" }
+        ]
+        });
+    });
+});
+  
